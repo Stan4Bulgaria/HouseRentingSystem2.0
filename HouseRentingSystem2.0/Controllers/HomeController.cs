6ff14df609b9c6234be0e33fb507ed760e93739a
Stanislav Lazov <stanislav_lazov1@abv.bg>
2025-05-22 00:18:01 +0300

    adding house service to the homectroller

--- HouseRentingSystem2.0/Controllers/HomeController.cs
+++ HouseRentingSystem2.0/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using HouseRentingSystem2._0.Core.Models.Home;
+﻿using HouseRentingSystem2._0.Core.Contracts.House;
+using HouseRentingSystem2._0.Core.Models.Home;
 using HouseRentingSystem2._0.Models;
 using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Manage.Internal;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +11,20 @@ namespace HouseRentingSystem2._0.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        private readonly IHouseService houseService;
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            IHouseService _houseService)
         {
             _logger = logger;
-            _configuration = configuration;
+            houseService = _houseService;
+
         }
 
         public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
-            ViewBag.Message = _configuration.GetValue<string>("EnvMessage");
+            var model = await houseService.LastThreeHouses();
             return View(model);
         }
 
